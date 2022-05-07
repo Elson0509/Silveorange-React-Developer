42f77fbc2661d37d225d87a7c43b65e3f3809819
Elson Ramos <32194122+Elson0509@users.noreply.github.com>
2022-05-07 17:25:47 -0400

    API - Filtering forks for false, setting content-type to 'application/json'

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -2,15 +2,23 @@ import { Router, Request, Response } from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
 import axios from 'axios';
+import { Repo } from '../models/Repo';
 export const repos = Router();
+
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
+  res.header('Content-Type', 'application/json');
 
   res.status(200);
 
   fs.readFile(path.join(__dirname, '../../data/repos.json'), (err, data) => {
-    // check error
+    if (err) {
+      res.status(500).json({
+        status: 500,
+        message: 'Internal Server Error',
+      });
+    }
     const jsonData = JSON.parse(data.toString());
 
     const url = 'https://api.github.com/users/silverorange/repos';
@@ -20,7 +28,9 @@ repos.get('/', async (_: Request, res: Response) => {
 
       const fullData = jsonData.concat(urlData);
 
-      return res.json(fullData);
+      const falseRepos = fullData.filter((el: Repo) => !el.fork);
+
+      return res.json(falseRepos);
     });
   });
 });
