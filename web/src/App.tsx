a0e12c1780d816c81e38e0e215ff8af72c2cf693
Elson Ramos <32194122+Elson0509@users.noreply.github.com>
2022-05-07 18:21:02 -0400

    WEB - Displaying buttons and filtering repositories by language

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function App() {
   const [repos, setRepos] = useState<any[]>([]);
+  const [filteredrepos, setFilteredRepos] = useState<any[]>([]);
+  const [languages, setLanguages] = useState<any[]>([]);
 
   useEffect(() => {
     axios
       .get('http://localhost:4000/repos')
       .then((res) => {
-        console.log(res.data);
-        setRepos(res.data);
+        const reposList = res.data;
+        const sortedInverseCronology = reposList.sort(
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          (a: { created_at: number }, b: { created_at: number }) =>
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        );
+        console.log(sortedInverseCronology);
+        console.log(listLanguages(sortedInverseCronology));
+        setLanguages(listLanguages(sortedInverseCronology));
+        setRepos(sortedInverseCronology);
+        setFilteredRepos(sortedInverseCronology);
       })
       .catch((err) => {
+        // TODO handle error and loading
         console.log(err);
       });
   }, []);
 
+  const listLanguages = (reposlist: any[]): [] => {
+    const languagesFilter = reposlist.reduce((acc, curr) => {
+      if (acc.includes(curr.language)) {
+        return acc;
+      } else {
+        return [...acc, curr.language];
+      }
+    }, []);
+    return languagesFilter;
+  };
+
+  const filterReposByLanguage = (language: string) => {
+    if (!language) {
+      setFilteredRepos(repos);
+    } else {
+      setFilteredRepos(repos.filter((repo: any) => repo.language === language));
+    }
+  };
+
   return (
     <main className="container-fluid">
       <div className="row">
-        {repos.map((repo) => (
+        <div className="col-12 py-4">
+          {languages.map((language: any) => (
+            <Button
+              variant="primary"
+              className="mx-2"
+              onClick={() => filterReposByLanguage(language)}
+              key={language}
+            >
+              {language}
+            </Button>
+          ))}
+        </div>
+      </div>
+      <div className="row">
+        {filteredrepos.map((repo) => (
           <Card key={repo.id} bg="light">
             <Card.Body>
               <Card.Title>{repo.name}</Card.Title>
