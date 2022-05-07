2bd6f936303d93e741c2a524bd51d3e500eca192
Elson Ramos <32194122+Elson0509@users.noreply.github.com>
2022-05-07 19:13:30 -0400

    WEB - Adding spinner when fetching data and message error when fetch is failed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Spinner, Alert } from 'react-bootstrap';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactMarkdown from 'react-markdown';
@@ -9,6 +9,8 @@ export function App() {
   const [repos, setRepos] = useState<any[]>([]);
   const [filteredrepos, setFilteredRepos] = useState<any[]>([]);
   const [languages, setLanguages] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [errorFetchingData, setErrorFetchingData] = useState(false);
 
   useEffect(() => {
     axios
@@ -20,15 +22,15 @@ export function App() {
           (a: { created_at: number }, b: { created_at: number }) =>
             new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
         );
-        console.log(sortedInverseCronology);
-        console.log(listLanguages(sortedInverseCronology));
         setLanguages(listLanguages(sortedInverseCronology));
         setRepos(sortedInverseCronology);
         setFilteredRepos(sortedInverseCronology);
       })
       .catch((err) => {
-        // TODO handle error and loading
-        console.log(err);
+        setErrorFetchingData(true);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
@@ -55,7 +57,6 @@ export function App() {
     axios
       .get(`https://raw.githubusercontent.com/${repoName}/master/README.md`)
       .then((res) => {
-        console.log(res.data);
         const reposCopy = [...filteredrepos];
         const newRepos = reposCopy.map((repo: any) => {
           if (repo.full_name === repoName) {
@@ -63,58 +64,73 @@ export function App() {
           }
           return repo;
         });
-        console.log({ newRepos });
         setFilteredRepos(newRepos);
       });
   };
 
+  if (loading) {
+    return <Spinner animation="border" variant="primary" />;
+  }
+
+  if (errorFetchingData) {
+    return (
+      <Alert variant="danger" className="text-center my-2">
+        Error fetching data from the server!
+      </Alert>
+    );
+  }
+
   return (
     <main className="container-fluid">
-      <div className="row">
-        <div className="col-12 py-4">
-          {languages.map((language: any) => (
+      <div>
+        <div className="row">
+          <div className="col-12 py-4">
+            {languages.map((language: any) => (
+              <Button
+                variant="primary"
+                className="mx-2"
+                onClick={() => filterReposByLanguage(language)}
+                key={language}
+              >
+                {language}
+              </Button>
+            ))}
             <Button
-              variant="primary"
+              variant="danger"
               className="mx-2"
-              onClick={() => filterReposByLanguage(language)}
-              key={language}
+              onClick={() => filterReposByLanguage('')}
             >
-              {language}
+              Original list
             </Button>
-          ))}
-          <Button
-            variant="danger"
-            className="mx-2"
-            onClick={() => filterReposByLanguage('')}
-          >
-            Original list
-          </Button>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        {filteredrepos.map((repo) => (
-          <Card
-            key={repo.id}
-            bg="light"
-            onClick={() => clickRepoHandler(repo.full_name)}
-            className="pointer card-repo"
-          >
-            <Card.Body>
-              <Card.Title>{repo.name}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Language: {repo.language}
-              </Card.Subtitle>
-              <Card.Text>Description: {repo.description}</Card.Text>
-              <Card.Text>Forks count: {repo.forks_count}</Card.Text>
-              {repo.markdown ? (
-                <div>
-                  <hr />
-                  <ReactMarkdown>{repo.markdown}</ReactMarkdown>
-                </div>
-              ) : null}
-            </Card.Body>
-          </Card>
-        ))}
+        <div className="row">
+          {filteredrepos.map((repo) => (
+            <Card
+              key={repo.id}
+              bg="light"
+              onClick={() => clickRepoHandler(repo.full_name)}
+              className="pointer card-repo"
+            >
+              <Card.Body>
+                <Card.Title>{repo.name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Language: {repo.language}
+                </Card.Subtitle>
+                <Card.Text>
+                  Description: {repo.description ?? 'No description provided.'}
+                </Card.Text>
+                <Card.Text>Forks count: {repo.forks_count}</Card.Text>
+                {repo.markdown ? (
+                  <div>
+                    <hr />
+                    <ReactMarkdown>{repo.markdown}</ReactMarkdown>
+                  </div>
+                ) : null}
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
       </div>
     </main>
   );
